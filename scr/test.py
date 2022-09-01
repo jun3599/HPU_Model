d1c774ea96d68a01a43bf3a68683caa582883fff
Junhwi <wnsgnl3599@naver.com>
2022-09-01 23:47:14 +0900

    [new util] 데이터 수집 현황을 파악하기 위한 프로그램 추가

--- scr/test.py
+++
@@ -1,9 +0,0 @@
-import os 
-import re 
-
-files = os.listdir(r'D:\users\Desktop\Junhwi\국토연구원\HPU_Model\data\raw\202201')
-files = [x.split('_')[0].strip() for x in files]
-
-target_press = ['서울신문','서울경제','한겨레','세계일보','머니투데이','문화일보','매일경제','국민일보','KBS','동아일보','경향신문','MBC','한국경제','SBS','YTN']
-
-print(sorted(list(set(target_press) - set(files)), reverse=False))
