75cc65de770b31d94126a17633e9f409f770d9cb
Junhwi <wnsgnl3599@naver.com>
2022-09-02 00:02:12 +0900

    [new function] 수집데이터의 무결성을 확인하기 위한 기능 추가

--- scr/refine.py
+++ scr/refine.py
@@ -1,8 +1,13 @@
+# 자연어 처리를 위한 모듈 
 import re 
+from konlpy.tag import Mecab 
+
 import pandas as pd 
 import sys
 from datetime import datetime 
-import os 
+import os
+
+from sklearn import model_selection 
 
 
 # 1. 컴파일 대상 폴더를 지정하도록 유도한다. 
@@ -22,9 +27,52 @@ class refine_data():
         self.HPU_dict_path = HPU_dict_path
         self.mecab_path = mecab_path 
         self.data_path = data_path 
+        self.tokenizer = Mecab(self.mecab_path)
         
+    def mode_selector(self):
+        mode = {'1': '1개 파일만 컴파일 진행', '2': '컴파일된 파일이 없는 모든 파일을 컴파일', '3': '여러개의 파일 선택 컴파일', '0': '모든 파일 컴파일'}
+        selected = input("컴파일 옵션을 선택해주세요! \n1: 1개 파일만 컴파일 진행\n2: 컴파일된 파일이 없는 모든 파일을 컴파일\n3: 여러개의 파일 선택 컴파일\n0: 모든 파일 컴파일\n (원하시는 번호를 입력후 엔터를 눌러주세요!): ")
 
-    # def mode_selector(self):
+        # 입력값 확인 
+        if selected not in ['0','1','2','3']:
+            sys.exit("잘못된 컴파일 옵션 선택입니다. 확인후 프로그램을 다시 시작해주세요!")
+
+        # 입력 재확인 
+        confirm = input(f"선택하신 컴파일 옵션이 \n\"{mode[selected]}\"가 맞습니까? (y/n): ")
+        if confirm == 'n':
+            self.mode_selector()
+        else: 
+            return selected 
+    
+    def allocator(self):
+        mode = self.mode_selector()
+        if mode == '1':
+            self.compile_option_1()
+        elif mode == '2':
+            self.compile_option_2()
+        elif mode == '3':
+            self.compile_option_3()
+        elif mode == '0':
+            self.compile_option_0()
+
+
+    def compile_option_1(self):
+        '''
+        1개 파일을 대상으로 컴파일을 진행합니다. 
+        입력의 형식은 yyyymm의 파일명입니다. 
+        '''
+        print("1개 파일 컴파일 옵션을 선택하셨습니다. ")
+        target_file_name = input('컴파일 대상 폴더의 이름을 yyyymm형식으로 정확히 입력후 Enter를 눌러주세요: ')
+        confirm = input(f'대상 폴더가 {target_file_name}이 맞습니까? (y/n): ')
+        return None
+
+    def compile_option_2(self):
+        return None
+
+    def compile_option_3(self):
+        return None
+    def compile_option_0(self):
+        return None 
 
     def confirm_compile_target(self):
         '''
@@ -35,7 +83,7 @@ class refine_data():
 
         return None 
 
-    def confirm_ready_compiled(self,file_name):
+    def confirm_ready_made(self,file_name):
         '''
         지정된 파일 경로에, 이미 컴파일 되어있는 파일이 존재할 경우 True를,
         존재하지 않을 경우 False를 반환합니다. 
@@ -70,15 +118,115 @@ class refine_data():
         return None 
 
     
-    def compile(self):
+    def compile(self, monthly_data_path, save_result_path):
 
+        data = self.merge_monthly_data(monthly_data_path=monthly_data_path)
+        data = self.find_group_of_word(data)
+        data = self.find_group_of_word(data)
+        data = self.write_word_group_TF(data)
+        data = self.grouping_article(data)
+
+        data.to_csv(save_result_path, index=False, encoding='utf-8-sig')
+
+        return None 
+
+    def merge_monthly_data(self, monthly_data_path):
+        data = pd.DataFrame()
+        for file in os.listdir(monthly_data_path):
+            
+            temp = pd.read_csv(f'{monthly_data_path/file}', encoding='utf-8-sig' ,error_bad_lines=False)
+            
+            # 언론사 이름 통일 
+            press = file.split('_')[0].strip()
+            temp['press'] = press 
+
+            # 기사 입력정보 정제 
+            temp.drop(columns=['input_date_y'], inplace=True)
+            temp.rename(columns={'input_date_x':'input_date'}, inplace=True)
+            temp['input_date'] = pd.to_datetime(temp['input_date'], errors='ignore')
+
+            # 기사 제목 정제 
+            temp.drop(columns=['title_y'], inplace=True)
+            temp.rename(columns={'title_x':'title'}, inplace=True)
+            temp['title'] = temp['title'].apply(lambda  x: re.sub(r'\n',' ', str(x))).apply(lambda  x: re.sub(r'\t',' ', str(x))).apply(lambda  x: re.sub(r'\s',' ', str(x)))
+
+            # 기사 본문 정제 
+            temp['article'] = temp['article'].apply(lambda  x: re.sub(r'\n',' ', str(x))).apply(lambda  x: re.sub(r'\t',' ', str(x))).apply(lambda  x: re.sub(r'\s',' ', str(x)))
+
+
+            data = data.append(temp, ignore_index= True)
         
+        return data  
 
-        return None 
 
 
-    def tokenize_article(self):
-        return None 
-    
+    def find_group_of_word(self, df):
+        '''
+        HPU 단어사전을 기반으로 
+        각 기사에서 각 단어군에 해당하는 단어들이 도출되면, 해당 내용을 H,P,U_A, U_R 컬럼에 담습니다. 
+        '''
+        def compare_article_list(tokenized_article_list, word_list):
+            tokenized_article_list = set(tokenized_article_list)
+            word_list = set(word_list)
 
-    
+            result = tokenized_article_list.intersection(word_list)
+            return list(result) 
+        
+        try:
+            target_dict = pd.read_excel(self.HPU_dict_path)
+        except FileNotFoundError as nf:
+            sys.exit(f'HPU사전이 지정된 경로에 존재하지 않습니다.  \nERROR CODE: {nf}')
+
+        H = target_dict.loc[target_dict['tag'] == 'H', 'word'].to_list()
+        P = target_dict.loc[target_dict['tag'] == 'P', 'word'].to_list()
+        U_A = target_dict.loc[target_dict['tag'] == 'U-A', 'word'].to_list()
+        U_R = target_dict.loc[target_dict['tag'] == 'U-R', 'word'].to_list()
+
+        df['H_word'] = df['tokenized_article'].apply(lambda x: compare_article_list(x, H))
+        df['P_word'] = df['tokenized_article'].apply(lambda x: compare_article_list(x, P))
+        df['U-A_word'] = df['tokenized_article'].apply(lambda x: compare_article_list(x, U_A))
+        df['U-R_word'] = df['tokenized_article'].apply(lambda x: compare_article_list(x, U_R))
+
+        return df 
+
+    def write_word_group_TF(self, df):
+        '''
+        find_group_of_word 함수를 통해 생성된 열들을 기반으로 
+        단어의 출현 여부를 확인해 T/F 값을 기입합니다. 
+        '''
+        def confirm_TF(word_list):
+            if len(word_list) == 0:
+                return 'F' 
+            else:
+                return 'T'
+
+        df['H_TF'] = df['H_word'].apply(lambda x: confirm_TF(x))
+        df['P_TF'] = df['P_word'].apply(lambda x: confirm_TF(x))
+        df['U-A_TF'] = df['U-A_word'].apply(lambda x: confirm_TF(x))
+        df['U-R_TF'] = df['U-R_word'].apply(lambda x: confirm_TF(x))
+
+        return df
+
+    def grouping_article(self, df):
+        '''
+        write_word_group_TF를 통해 도출된 내역을 기반으로 
+        HPU-uncertainity 와 HPU-Risk에 해당 하는 단어군을 파악해 TF로 기록합니다. 
+
+        '''
+        def confirm_HPU_A_TF(H_TF, P_TF, UA_TF):
+            if (H_TF == 'T') & (P_TF == 'T') & (UA_TF == 'T'):
+              HPU_A_TF = 'T'
+            else:
+                HPU_A_TF = 'F'
+            return HPU_A_TF
+        def confirm_HPU_R_TF(H_TF, P_TF, UR_TF):
+            if (H_TF == 'T') & (P_TF == 'T') & (UR_TF == 'T'):
+                HPU_R_TF = 'T'
+            else:
+                HPU_R_TF = 'F'
+            return HPU_R_TF
+
+        df['HPU_A_TF'] = df.apply(lambda x: confirm_HPU_A_TF(x['H_TF'],x['P_TF'],x['U-A_TF']), axis =1)
+        df['HPU_R_TF'] = df.apply(lambda x: confirm_HPU_R_TF(x['H_TF'],x['P_TF'],x['U-R_TF']), axis =1)
+        
+        return df 
