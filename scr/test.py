d730ea79dc33364bc175bfd47855fbb2c42a522b
JunHwi Park <wnsgnl3599@naver.com>
2022-09-02 17:38:38 +0900

    [test]: 지표계산 테스트

--- scr/test.py
+++ scr/test.py
@@ -1,3 +1,2 @@
-from refine import * 
-
-refine_data(HPU_dict_path='D:/users/Desktop/Junhwi/국토연구원/HPU_Model/HPU_dict.xlsx', mecab_ko_dict_path='C:/mecab/mecab-ko-dic', data_path= 'D:/users/Desktop/Junhwi/국토연구원/HPU_Model/data')
+from calculate_index import * 
+calculate_index('D:/Users/wnsgnl/Desktop/paper_master2/HPU_Model/data')
